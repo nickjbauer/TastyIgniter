6bd38b47d3e6e73bd14722272922f1a8a86706d2
Nick Bauer <nick.j.bauer@gmail.com>
2017-06-24 21:19:50 +0000

    add animation to the eta update

--- extensions/local_module/views/assets/real-time-delivery.js.js
+++ extensions/local_module/views/assets/real-time-delivery.js.js
@@ -1,6 +1,17 @@
 jQuery(function() {
     console.log("Pizza Pronto Real-time");
 
+    function pulsate() {
+      $(".estimatedWaitForDelivery").
+      css({"font-weight": "bolder"}).
+      animate({opacity: 0.5, "font-weight": "lighter", "font-size": "80%"}, 100, 'linear').
+      animate({opacity: 0.75, "font-weight": "bolder", "font-size": "120%"}, 100, 'linear').
+      animate({opacity: 1, "font-weight": "normal", "font-size": "100%"}, 100, 'linear');
+      
+//        css({opacity: 0.2, 'font-weight':"bolder", 'font-size': "300%"}). 
+//        animate({opacity: 1, 'font-size': "150%"}, 750, 'linear');
+    }
+
     var updateEta = function(){
         
         var min = Math.ceil(1);
@@ -18,8 +29,9 @@ jQuery(function() {
         var formatString = $("#text_in_minutes").text();
         
         $(".estimatedWaitForDelivery").text(formatString.format(rando));
+        pulsate();
     };
-
-    window.setInterval(updateEta, 1000);
+    
+    window.setInterval(updateEta, 5000);
 
 });
