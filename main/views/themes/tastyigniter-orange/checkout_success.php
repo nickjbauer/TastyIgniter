6228680393368b159704457909f9a50abbd99d20
Nick Bauer <nick.j.bauer@gmail.com>
2017-06-25 17:58:30 +0000

    remove unnecessary 'location' column in success page

--- main/views/themes/tastyigniter-orange/checkout_success.php
+++ main/views/themes/tastyigniter-orange/checkout_success.php
@@ -38,20 +38,15 @@
 			                    <h3 class="panel-title"><strong><?php echo lang('text_order_details'); ?></strong></h3>
 			                </div>
 			                <div class="panel-body">
-			                    <div class="col-sm-4">
+			                    <div class="col-sm-6">
 			                        <p><?php echo $order_details; ?></p>
 			                    </div>
-			                    <div class="col-sm-4">
+			                    <div class="col-sm-6">
 									<?php if ($delivery_address) { ?>
 										<strong><?php echo lang('text_delivery_address'); ?>:</strong>
 										<address><?php echo $delivery_address; ?></address>
 									<?php } ?>
 			                    </div>
-								<div class="col-sm-4">
-									<strong><?php echo lang('text_your_local'); ?></strong><br />
-									<strong><?php echo $location_name; ?></strong><br />
-									<address><?php echo $location_address; ?></address>
-								</div>
 			                </div>
 			            </div>
 
